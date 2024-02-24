136c2dd4ab708fa00b9a39951652d7c1a713612b
sruthykpradeep <sruthykpradeep@gmail.com>
2024-02-24 16:16:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Loginsignup from './components/Assets/Loginsignup/Loginsignup';
+
+
+
+function App() {
+  return (
+    <div>
+      <Loginsignup/>
+    </div>
+      
+        
+          
+          
+      
+  );
+}
+
+export default App;
